8e1e7c214cbe6c121cd4dd7d0dafaad51e340ae0
keenonkites <keenonkites@gmx.net>
2014-11-04 23:44:58 +0100

    additional output

--- elevation/create-elevation-data-bg.bash
+++ elevation/create-elevation-data-bg.bash
@@ -22,12 +22,14 @@ echo "============================================="
 
 PBFPREFIX=$(basename $POLYFILE .poly)
 
+echo "25/250/500 m"
+echo "---------------------------------------------"
 phyghtmap --step=25                         \
           --osm-version=0.6                 \
           --jobs=1                          \
           --polygon=$POLYFILE               \
           --line-cat=500,250                \
-          --source=view1,srtm1,view3,srtm3  \
+          --source=view1,view3,srtm1,srtm3  \
           --start-node-id=$NID              \
           --start-way-id=$WID               \
           --max-nodes-per-tile=0            \
@@ -37,12 +39,15 @@ phyghtmap --step=25                         \
 
 mv ./pbf/ele_25_250_500/Hoehendaten_${1}*.osm.pbf ./pbf/ele_25_250_500/Hoehendaten_${1}.osm.pbf
 
+echo 
+echo "10/100/200 m"
+echo "---------------------------------------------"
 phyghtmap --step=10                         \
           --osm-version=0.6                 \
           --jobs=1                          \
           --polygon=$POLYFILE               \
           --line-cat=200,100                \
-          --source=view1,srtm1,view3,srtm3  \
+          --source=view1,view3,srtm1,srtm3  \
           --start-node-id=$NID              \
           --start-way-id=$WID               \
           --max-nodes-per-tile=0            \
@@ -52,3 +57,4 @@ phyghtmap --step=10                         \
 		  
 mv ./pbf/ele_10_100_200/Hoehendaten_${1}*.osm.pbf ./pbf/ele_10_100_200/Hoehendaten_${1}.osm.pbf
 
+echo
