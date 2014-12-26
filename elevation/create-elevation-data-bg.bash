cb464adc990dbe3eefbbc05d83e2b1a298d568f0
keenonkites <keenonkites@gmx.net>
2014-12-26 16:33:40 +0100

    cleanup variable calles

--- elevation/create-elevation-data-bg.bash
+++ elevation/create-elevation-data-bg.bash
@@ -6,7 +6,7 @@ ELE10="ele_10_100_200"
 ELE20="ele_20_100_500"
 ELE25="ele_25_250_500"
 DATASRC="view1,view3"
-#DATASRC="view1,view3,SRTM1,SRTM3"
+#DATASRC="view1,view3,srtm1,srtm3"
 
 if [ -z "$2" ]
 then
@@ -31,21 +31,21 @@ LOGFILE="logs/$MAPNAME-ele20.log"
 phyghtmap --step=20                         \
           --osm-version=0.6                 \
           --jobs=1                          \
-          --polygon=$POLYFILE               \
+          --polygon=${POLYFILE}             \
           --line-cat=500,100                \
           --source=${DATASRC}               \
-          --start-node-id=$NID              \
-          --start-way-id=$WID               \
+          --start-node-id=${NID}            \
+          --start-way-id=${WID}             \
           --max-nodes-per-tile=0            \
           --pbf                             \
           --write-timestamp                 \
           --output-prefix=./pbf/${ELE20}/Hoehendaten_${MAPNAME} | tee ${LOGFILE}
 
-if [ -f ./pbf/$ELE20/Hoehendaten_${MAPNAME}.osm.pbf ]
+if [ -f ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf ]
 then
-   rm ./pbf/$ELE20/Hoehendaten_${MAPNAME}.osm.pbf
+   rm ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf
 fi
-mv ./pbf/$ELE20/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf
+mv ./pbf/${ELE20}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf
 
 echo
 echo "25/250/500 m"
@@ -54,21 +54,21 @@ LOGFILE="logs/$MAPNAME-ele25.log"
 phyghtmap --step=25                         \
           --osm-version=0.6                 \
           --jobs=1                          \
-          --polygon=$POLYFILE               \
+          --polygon=${POLYFILE}             \
           --line-cat=500,250                \
           --source=${DATASRC}               \
-          --start-node-id=$NID              \
-          --start-way-id=$WID               \
+          --start-node-id=${NID}            \
+          --start-way-id=${WID}             \
           --max-nodes-per-tile=0            \
           --pbf                             \
           --write-timestamp                 \
-          --output-prefix=./pbf/$ELE25/Hoehendaten_${MAPNAME} | tee ${LOGFILE}
+          --output-prefix=./pbf/${ELE25}/Hoehendaten_${MAPNAME} | tee ${LOGFILE}
 
-if [ -f ./pbf/$ELE25/Hoehendaten_${MAPNAME}.osm.pbf ]
+if [ -f ./pbf/${ELE25}/Hoehendaten_${MAPNAME}.osm.pbf ]
 then
-   rm ./pbf/$ELE25/Hoehendaten_${MAPNAME}.osm.pbf
+   rm ./pbf/${ELE25}/Hoehendaten_${MAPNAME}.osm.pbf
 fi
-mv ./pbf/$ELE25/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/$ELE25/Hoehendaten_${MAPNAME}.osm.pbf
+mv ./pbf/${ELE25}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/$ELE25/Hoehendaten_${MAPNAME}.osm.pbf
 
 
 echo 
@@ -81,17 +81,17 @@ phyghtmap --step=10                         \
           --polygon=$POLYFILE               \
           --line-cat=200,100                \
           --source=${DATASRC}               \
-          --start-node-id=$NID              \
-          --start-way-id=$WID               \
+          --start-node-id=${NID}            \
+          --start-way-id=${WID}             \
           --max-nodes-per-tile=0            \
           --pbf                             \
           --write-timestamp                 \
-          --output-prefix=./pbf/$ELE10/Hoehendaten_${MAPNAME} | tee ${LOGFILE}
+          --output-prefix=./pbf/${ELE10}/Hoehendaten_${MAPNAME} | tee ${LOGFILE}
 		  
-if [ -f ./pbf/$ELE10/Hoehendaten_${MAPNAME}.osm.pbf ]
+if [ -f ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf ]
 then
-   rm ./pbf/$ELE10/Hoehendaten_${MAPNAME}.osm.pbf
+   rm ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf
 fi
-mv ./pbf/$ELE10/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/$ELE10/Hoehendaten_${MAPNAME}.osm.pbf
+mv ./pbf/${ELE10}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf
 
 echo
