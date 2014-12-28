de18a9ca4d75d8d3848585439ccbef74203701ed
keenonkites <keenonkites@gmx.net>
2014-12-28 16:32:27 +0100

    moving phyghtmap call into function

--- elevation/create-elevation-data-bg.bash
+++ elevation/create-elevation-data-bg.bash
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+function createcontour {
+  
+  ELESTEP=$1
+  ELECAT=$2
+  ELEPATH=$3
+  LOGFILE="logs/$MAPNAME-ele$ELESTEP.log"
+  
+  phyghtmap --step=${ELESTEP}                 \
+            --osm-version=0.6                 \
+            --jobs=1                          \
+            --polygon=${POLYFILE}             \
+            --line-cat=${ELECAT}              \
+            --source=${DATASRC}               \
+            --start-node-id=${NID}            \
+            --start-way-id=${WID}             \
+            --max-nodes-per-tile=0            \
+            --pbf                             \
+            --write-timestamp                 \
+            --output-prefix=./pbf/${ELEPATH}/Hoehendaten_${MAPNAME} | tee >(grep using | grep hgt | awk '{print $NF}' | sed 's/.$//' > $LOGFILE)
+  
+  
+  if [ -f ./pbf/${ELEPATH}/Hoehendaten_${MAPNAME}.osm.pbf ]
+  then
+     rm ./pbf/${ELEPATH}/Hoehendaten_${MAPNAME}.osm.pbf
+  fi
+  mv ./pbf/${ELEPATH}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/${ELEPATH}/Hoehendaten_${MAPNAME}.osm.pbf
+  
+}
+
 MAPNAME="$1"
 POLYFILE="./poly/$MAPNAME.poly"
 ELE10="ele_10_100_200"
@@ -25,74 +54,88 @@ fi
 echo 
 echo "$POLYFILE"
 echo "============================================="
+
 echo "20/100/500 m"
 echo "---------------------------------------------"
-LOGFILE="logs/$MAPNAME-ele20.log"
-phyghtmap --step=20                         \
-          --osm-version=0.6                 \
-          --jobs=1                          \
-          --polygon=${POLYFILE}             \
-          --line-cat=500,100                \
-          --source=${DATASRC}               \
-          --start-node-id=${NID}            \
-          --start-way-id=${WID}             \
-          --max-nodes-per-tile=0            \
-          --pbf                             \
-          --write-timestamp                 \
-          --output-prefix=./pbf/${ELE20}/Hoehendaten_${MAPNAME} | tee >(grep using | grep hgt | awk '{print $NF}' | sed 's/.$//' > $LOGFILE)
-
-
-if [ -f ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf ]
-then
-   rm ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf
-fi
-mv ./pbf/${ELE20}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf
+createcontour "20" "500,100" "$ELE20"
 
 echo
 echo "25/250/500 m"
 echo "---------------------------------------------"
-LOGFILE="logs/$MAPNAME-ele25.log"
-phyghtmap --step=25                         \
-          --osm-version=0.6                 \
-          --jobs=1                          \
-          --polygon=${POLYFILE}             \
-          --line-cat=500,250                \
-          --source=${DATASRC}               \
-          --start-node-id=${NID}            \
-          --start-way-id=${WID}             \
-          --max-nodes-per-tile=0            \
-          --pbf                             \
-          --write-timestamp                 \
-          --output-prefix=./pbf/${ELE25}/Hoehendaten_${MAPNAME} | tee >(grep using | grep hgt | awk '{print $NF}' | sed 's/.$//' > $LOGFILE)
+createcontour "25" "500,250" "$ELE20"
 
-if [ -f ./pbf/${ELE25}/Hoehendaten_${MAPNAME}.osm.pbf ]
-then
-   rm ./pbf/${ELE25}/Hoehendaten_${MAPNAME}.osm.pbf
-fi
-mv ./pbf/${ELE25}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/$ELE25/Hoehendaten_${MAPNAME}.osm.pbf
-
-
-echo 
 echo "10/100/200 m"
 echo "---------------------------------------------"
-LOGFILE="logs/$MAPNAME-ele10.log"
-phyghtmap --step=10                         \
-          --osm-version=0.6                 \
-          --jobs=1                          \
-          --polygon=$POLYFILE               \
-          --line-cat=200,100                \
-          --source=${DATASRC}               \
-          --start-node-id=${NID}            \
-          --start-way-id=${WID}             \
-          --max-nodes-per-tile=0            \
-          --pbf                             \
-          --write-timestamp                 \
-          --output-prefix=./pbf/${ELE10}/Hoehendaten_${MAPNAME} | tee >(grep using | grep hgt | awk '{print $NF}' | sed 's/.$//' > $LOGFILE)
-		  
-if [ -f ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf ]
-then
-   rm ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf
-fi
-mv ./pbf/${ELE10}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf
+createcontour "10" "200,100" "$ELE20"
 
-echo
+
+#LOGFILE="logs/$MAPNAME-ele20.log"
+#phyghtmap --step=20                         \
+#          --osm-version=0.6                 \
+#          --jobs=1                          \
+#          --polygon=${POLYFILE}             \
+#          --line-cat=500,100                \
+#          --source=${DATASRC}               \
+#          --start-node-id=${NID}            \
+#          --start-way-id=${WID}             \
+#          --max-nodes-per-tile=0            \
+#          --pbf                             \
+#          --write-timestamp                 \
+#          --output-prefix=./pbf/${ELE20}/Hoehendaten_${MAPNAME} | tee >(grep using | grep hgt | awk '{print $NF}' | sed 's/.$//' > $LOGFILE)
+#
+#
+#if [ -f ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf ]
+#then
+#   rm ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf
+#fi
+#mv ./pbf/${ELE20}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/${ELE20}/Hoehendaten_${MAPNAME}.osm.pbf
+#
+#echo
+#echo "25/250/500 m"
+#echo "---------------------------------------------"
+#LOGFILE="logs/$MAPNAME-ele25.log"
+#phyghtmap --step=25                         \
+#          --osm-version=0.6                 \
+#          --jobs=1                          \
+#          --polygon=${POLYFILE}             \
+#          --line-cat=500,250                \
+#          --source=${DATASRC}               \
+#          --start-node-id=${NID}            \
+#          --start-way-id=${WID}             \
+#          --max-nodes-per-tile=0            \
+#          --pbf                             \
+#          --write-timestamp                 \
+#          --output-prefix=./pbf/${ELE25}/Hoehendaten_${MAPNAME} | tee >(grep using | grep hgt | awk '{print $NF}' | sed 's/.$//' > $LOGFILE)
+#
+#if [ -f ./pbf/${ELE25}/Hoehendaten_${MAPNAME}.osm.pbf ]
+#then
+#   rm ./pbf/${ELE25}/Hoehendaten_${MAPNAME}.osm.pbf
+#fi
+#mv ./pbf/${ELE25}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/$ELE25/Hoehendaten_${MAPNAME}.osm.pbf
+#
+#
+#echo 
+#echo "10/100/200 m"
+#echo "---------------------------------------------"
+#LOGFILE="logs/$MAPNAME-ele10.log"
+#phyghtmap --step=10                         \
+#          --osm-version=0.6                 \
+#          --jobs=1                          \
+#          --polygon=$POLYFILE               \
+#          --line-cat=200,100                \
+#          --source=${DATASRC}               \
+#          --start-node-id=${NID}            \
+#          --start-way-id=${WID}             \
+#          --max-nodes-per-tile=0            \
+#          --pbf                             \
+#          --write-timestamp                 \
+#          --output-prefix=./pbf/${ELE10}/Hoehendaten_${MAPNAME} | tee >(grep using | grep hgt | awk '{print $NF}' | sed 's/.$//' > $LOGFILE)
+#		  
+#if [ -f ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf ]
+#then
+#   rm ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf
+#fi
+#mv ./pbf/${ELE10}/Hoehendaten_${MAPNAME}*.osm.pbf ./pbf/${ELE10}/Hoehendaten_${MAPNAME}.osm.pbf
+#
+#echo
+#
