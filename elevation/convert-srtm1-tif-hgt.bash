f6089086ae7a15b3ddee908af5a835b093a42e67
keenonkites <keenonkites@gmx.net>
2023-11-25 13:06:17 +0100

    fix: correct location for hgt file

--- elevation/convert-srtm1-tif-hgt.bash
+++ elevation/convert-srtm1-tif-hgt.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+tif_dir=${$1%/}
+hgt_dir="${tif_dir}.hgt"
+mkdir $hgt_dir
+
 for tif_file in $1/*.tif
 do
     hgt_file="`basename $tif_file .tif`.hgt"
-    hgt_dir="`dirname $tif_file`.hgt"
-    gdal_translate -of SRTMHGT $tif_file $hgt_file
+    gdal_translate -of SRTMHGT $tif_file $hgt_dir/$hgt_file
 done
