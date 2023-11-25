9b7211a1c0fd8c26a717ec0efafc3bc22fa8e977
keenonkites <keenonkites@gmx.net>
2023-11-25 13:24:35 +0100

    adapting .gitignore and enhance hgt conversion

--- elevation/convert-srtm1-tif-hgt.bash
+++ elevation/convert-srtm1-tif-hgt.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-tif_dir=${$1%/}
+tif_dir=${1%/}
 hgt_dir="${tif_dir}.hgt"
+echo "SRC DIR tif files: $tif_dir"
+echo "DST DIR hgt files: $hgt_dir"
 mkdir $hgt_dir
 
 for tif_file in $1/*.tif
 do
     hgt_file="`basename $tif_file .tif`.hgt"
+    echo " $tif_file -> $hgt_dir/$hgt_file"
     gdal_translate -of SRTMHGT $tif_file $hgt_dir/$hgt_file
 done
